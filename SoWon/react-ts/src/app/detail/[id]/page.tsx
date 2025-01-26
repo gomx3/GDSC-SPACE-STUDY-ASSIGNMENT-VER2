973d50697b1556587d4ab671637d574ef0cd0ad9
Sowon Kim <sso626262@naver.com>
2025-01-26 19:17:37 +0900

    feat: 일기 상세 페이지 UI 구조 구현 (#31)

--- SoWon/react-ts/src/app/detail/[id]/page.tsx
+++ SoWon/react-ts/src/app/detail/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Diary } from '../../../interface/diary'
 import { formatDate } from '../../../utils/formatDate'
 
@@ -6,12 +6,12 @@ type DiaryDetailPageParams = {
     id: string
 }
 
-// 일기 더미 데이터
+// 일기 더미 데이터 (삭제 예정)
 const dummyDiary: Diary = {
     id: '1',
     date: new Date(),
     title: '나의 일기',
-    content: '오늘따라 거리에 사람이 없습니다. 이곳은 아포칼립스 세계관일까요?',
+    content: '오늘따라 거리에 사람이 없습니다.',
     emotion: 'soso',
     weather: 'cloud',
     views: 10,
@@ -23,15 +23,23 @@ export default function DiaryDetailPage() {
     return (
         <div className="min-h-screen max-h-screen h-screen w-full bg-white flex items-center justify-center">
             <div className="w-1/2 h-full py-20">
-                <div className="flex flex-col gap-4 my">
+                <div className="flex flex-col gap-4 my-9">
                     <h1 className="text-4xl font-medium">{dummyDiary.title}</h1>
-                    <div className="flex flex-row">
-                        <div>{formatDate(dummyDiary.date)}</div>
-                        <div>{dummyDiary.weather}</div>
-                        <div>{dummyDiary.emotion}</div>
+                    <div className="flex flex-row gap-2">
+                        <div className="btn btn-gray w-full px-1.5 py-0.5 text-sm">
+                            {formatDate(dummyDiary.date, 'verbose')}
+                        </div>
+                        <div className="btn btn-gray w-full px-1.5 py-0.5 text-sm">{dummyDiary.weather}</div>
+                        <div className="btn btn-gray w-full px-1.5 py-0.5 text-sm">{dummyDiary.emotion}</div>
                     </div>
                 </div>
-                <div>{dummyDiary.content}</div>
+                <div className="text-base text-gray-800 h-2/3">{dummyDiary.content}</div>
+                <div className="flex flex-row gap-2">
+                    <Link to={'/'} className="btn btn-green">
+                        새로운 일기 작성하기
+                    </Link>
+                    <button className="btn btn-red">현재 일기 삭제하기</button>
+                </div>
             </div>
         </div>
     )
--- SoWon/react-ts/src/utils/formatDate.ts
+++ SoWon/react-ts/src/utils/formatDate.ts
@@ -1,8 +1,20 @@
-export const formatDate = (date: Date | string) => {
+type option = 'default' | 'verbose'
+
+export const formatDate = (date: Date | string, format: option) => {
     if (typeof date === 'string') {
         date = new Date(date)
     }
 
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString()
+    const day = date.getDate().toString()
+
+    // option이 verbose
+    if (format === 'verbose') {
+        return `${year}년 ${month}월 ${day}일`
+    }
+
+    // option이 default
     return new Intl.DateTimeFormat('ko-KR', {
         year: 'numeric',
         month: '2-digit',
